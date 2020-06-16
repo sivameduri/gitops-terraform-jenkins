83b4b9f152295208f5f1ea4debc42f4dc1691011
sivameduri <38209611+sivameduri@users.noreply.github.com>
2020-06-16 12:11:58 +0530

    New main.tf and variables.tf
    added new files to repo main.tf and variables.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
-    key    = "terraform.tfstate"
+    bucket = "terrafrombucketsiva"
+    key    = "DevopsMainKey"
     region = "us-east-1"
   }
 }
--- variables.tf
+++ variables.tf
@@ -16,5 +16,5 @@ variable "ami" {
   description = "Base AMI to launch the instances"
 
   # Bitnami NGINX AMI
-  default = "ami-021acbdb89706aa89"
+  default = "ami-05126bc2ea74427e6"
 }
